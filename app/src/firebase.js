8d59bb3d30ed7feeec13a8a84770872250f33dfe
charles-vj <71175155+charles-vj@users.noreply.github.com>
2021-05-21 14:27:35 +0530

    Update firebase.js

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBfpc5E8qSu7QYi5L76QcIWi3p3TcwWHpk",
-    authDomain: "reacc-auth.firebaseapp.com",
-    projectId: "reacc-auth",
-    storageBucket: "reacc-auth.appspot.com",
-    messagingSenderId: "745219778052",
-    appId: "1:745219778052:web:6e5741a143beb9e03fb751",
-    measurementId: "G-7JVS20F9JM"
+    apiKey: "keyhere",
+    authDomain: "domainhere",
+    projectId: "projectIdhere",
+    storageBucket: "buckethere",
+    messagingSenderId: "msidhere",
+    appId: "appidhere",
+    measurementId: "msmentidhere"
 
 })
 
